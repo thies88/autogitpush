74bffb5bd6f5a3df0c3c453fa3b0e9152132ed48
thies88 <thijsheijdemann@gmail.com>
2021-02-23 18:27:48 +0100

    Update script.sh

--- root/opt/script.sh
+++ root/opt/script.sh
@@ -7,9 +7,14 @@ for image in * ; do
 
   cd $gitfolder/$image || exit
 
+  # update repo
+  git pull
+  #add tekst to txt
   date >> $gitfolder/$image/weeklybuild.txt
+  # commit changes
   git commit -a -m "Update image"
-
+  
+  #push commit to github
   git push origin master
   #origin/master
 done
